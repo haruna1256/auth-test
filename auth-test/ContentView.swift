0c23785d8db83751227f0b07a1bebe7c1c634939
haruna1256 <haru_kawagishi@icloud.com>
2025-06-02 10:02:52 +0900

    Initial Commit

---
+++ auth-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  auth-test
+//
+//  Created by 川岸遥奈 on 2025/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ auth-test/auth_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  auth_testApp.swift
+//  auth-test
+//
+//  Created by 川岸遥奈 on 2025/06/02.
+//
+
+import SwiftUI
+
+@main
+struct auth_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
